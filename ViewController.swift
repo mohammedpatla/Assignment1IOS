2dd12cd094835ef467b20670cc7a07b8f6940d65
mohammedpatla <mohammedtaher4patla@gmail.com>
2018-10-10 12:12:50 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1IOS
+//
+//  Created by Mohammed Patla on 2018-10-10.
+//  Copyright © 2018 Mohammed Patla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
